d82f5ed592c064e8307dca87b7e701d3c93858eb
j4ckris <frenshystyle255@protonmail.com>
2024-08-31 05:22:43 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 import bencodepy
 import hashlib
 import requests
+import socket
 # Examples:
 #
 # - decode_bencode(b"5:hello") -> b"hello"
@@ -96,6 +97,26 @@ def main():
             ip_address = f"{peer[0]}.{peer[1]}.{peer[2]}.{peer[3]}"
             port = int.from_bytes(peer[4:], byteorder="big", signed=False)
             print(f"{ip_address}:{port}")
+
+    elif command == "handshake":
+        file_name = sys.argv[2]
+        (ip, port) = sys.argv[3].split(":")
+        with open(file_name, "rb") as file:
+            parsed = decode_bencode(file.read())
+            info = parsed["info"]
+            bencoded_info = bencodepy.encode(info)
+            info_hash = hashlib.sha1(bencoded_info).digest()
+            handshake = (
+                b"\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00"
+                + info_hash
+                + b"00112233445566778899"
+            )
+            # make request to peer
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((ip, int(port)))
+                s.send(handshake)
+                print(f"Peer ID: {s.recv(68)[48:].hex()}")
+
 
     else:
         raise NotImplementedError(f"Unknown command {command}")
